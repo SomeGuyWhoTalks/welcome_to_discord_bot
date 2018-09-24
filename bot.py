10b4d37a0a9253036768a3bd4d7dd46e59ddcdff
SomeGuyWhoTalks <sreyes103@gmail.com>
2018-09-24 11:10:59 -0700

    Python Script
    My Python Script for Discord bot

---
+++ bot.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+
+client = discord.Client()
+
+
+
+newUserMessage = """
+
+welcome to this server!
+
+"""
+
+@client.event
+async def on_ready():
+	print('logged in')
+	print(client.user.name)
+	print(client.user.id)
+	print('-------------')
+
+@client.event
+async def on_member_join(member):
+    print("Recognised that a member called " + member.name + " joined")
+    await client.send_message(client.get_channel("490829315026583574"), newUserMessage)
+    print("Sent message to " + member.name)
+
+    role = discord.utils.get(member.server.roles, name="ROLE")
+    await client.add_roles(member, role)
+
+@client.event
+async def on_message(message):
+	if message.content.startswith('!hello'):
+		await client.send_message(message.channel, 'hello')
+		msg = await client.wait_for_message(author=message.author, content='Hello.')
+		await client.send_message(message.channel, 'Hello.')
+
+client.run('TOKEN')
